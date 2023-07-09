df522deb71b734e51b3ad350c3876ffefd5c3d74
Yuri <yurideoliveirac@gmail.com>
2023-07-08 22:35:33 -0300

    Finalizada as questões funcionais do app

--- app/src/main/java/com/example/ppt/MainActivity.java
+++ app/src/main/java/com/example/ppt/MainActivity.java
@@ -17,32 +17,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    ///////////*SELEÇÕES DO JOGADOR*/////////////
+
     public int selecaoPedra(View view){
         TextView escolha = findViewById(R.id.escolha);
-        escolha.setText("VOCÊ ESCOLHEU PEDRA!");
-        TextView resultado = findViewById(R.id.resultado);
-        resultado.setText("");
+        escolha.setText("VOCÊ ESCOLHEU PEDRA");
         int r = selecaoComputador(view);
+        Resultado(1, r);
         return 1;
     }
 
     public int selecaoPapel(View view){
         TextView escolha = findViewById(R.id.escolha);
-        escolha.setText("VOCÊ ESCOLHEU PAPEL!");
-        TextView resultado = findViewById(R.id.resultado);
-        resultado.setText("");
+        escolha.setText("VOCÊ ESCOLHEU PAPEL");
         int r = selecaoComputador(view);
+        Resultado(2, r);
         return 2;
     }
 
     public int selecaoTesoura(View view){
         TextView escolha = findViewById(R.id.escolha);
-        escolha.setText("VOCÊ ESCOLHEU TESOURA!");
-        TextView resultado = findViewById(R.id.resultado);
-        resultado.setText("");
+        escolha.setText("VOCÊ ESCOLHEU TESOURA");
         int r = selecaoComputador(view);
+        Resultado(3, r);
         return 3;
     }
+
+    ///////////*SELEÇÕES DO COMPUTADOR*/////////////
 
     public int selecaoComputador(View view){
         int i = new Random().nextInt(3);
@@ -50,19 +51,52 @@ public class MainActivity extends AppCompatActivity {
             ImageView icon_selec_com = findViewById(R.id.icone_selec_com);
             icon_selec_com.setImageResource(R.drawable.com_pedra);
             TextView escolha = findViewById(R.id.escolha_com);
-            escolha.setText("O COMPUTADOR ESCOLHEU PEDRA!");
+            escolha.setText("O COMPUTADOR ESCOLHEU PEDRA");
         }else if(i == 2){
             ImageView icon_selec_com = findViewById(R.id.icone_selec_com);
             icon_selec_com.setImageResource(R.drawable.com_papel);
             TextView escolha = findViewById(R.id.escolha_com);
-            escolha.setText("O COMPUTADOR ESCOLHEU PAPEL!");
+            escolha.setText("O COMPUTADOR ESCOLHEU PAPEL");
         }else{
             ImageView icon_selec_com = findViewById(R.id.icone_selec_com);
             icon_selec_com.setImageResource(R.drawable.com_tesoura);
             TextView escolha = findViewById(R.id.escolha_com);
-            escolha.setText("O COMPUTADOR ESCOLHEU TESOURA!");
+            escolha.setText("O COMPUTADOR ESCOLHEU TESOURA");
         }
         return i;
+    }
+
+    ///////////*RESULTADOS*/////////////
+
+    public void Resultado(int jogador, int computador){
+        if(jogador == computador){
+            TextView resultado = findViewById(R.id.resultado);
+            resultado.setText("EMPATE!");
+        }else if(jogador == 1){
+            if(computador == 2){
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ PERDEU.");
+            }else{
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ GANHOU!");
+            }
+        }else if(jogador == 2){
+            if(computador == 3){
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ PERDEU.");
+            }else{
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ GANHOU!");
+            }
+        }else{
+            if(computador == 1){
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ PERDEU.");
+            }else{
+                TextView resultado = findViewById(R.id.resultado);
+                resultado.setText("VOCÊ GANHOU!");
+            }
+        }
     }
 
 }
